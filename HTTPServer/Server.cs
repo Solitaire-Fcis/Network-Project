5e966b574292b20e6aa9a8389ef00f6d3bc6523c
Youssefanter <yooiialy@gmail.com>
2021-12-16 00:50:29 +0200

    Half Server Without Testing

--- HTTPServer/Server.cs
+++ HTTPServer/Server.cs
@@ -17,15 +17,24 @@ namespace HTTPServer
         {
             //TODO: call this.LoadRedirectionRules passing redirectionMatrixPath to it
             //TODO: initialize this.serverSocket
+            this.LoadRedirectionRules(redirectionMatrixPath);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint hostEndPoint = new IPEndPoint(IPAddress.Any, portNumber);
+            serverSocket.Bind(hostEndPoint);
+            byte[] data;
         }
 
         public void StartServer()
         {
             // TODO: Listen to connections, with large backlog.
-
+            serverSocket.Listen(1000);
             // TODO: Accept connections in while loop and start a thread for each connection on function "Handle Connection"
             while (true)
             {
+                Socket clientSocket = this.serverSocket.Accept();
+                Console.WriteLine("New Client Just Landed On Our Server:{0}", clientSocket.RemoteEndPoint);
+                Thread OpenNewThread = new Thread(new ParameterizedThreadStart(HandleConnection));
+                OpenNewThread.Start(clientSocket);
                 //TODO: accept connections and start thread for each accepted connection.
 
             }
@@ -35,20 +44,30 @@ namespace HTTPServer
         {
             // TODO: Create client socket 
             // set client socket ReceiveTimeout = 0 to indicate an infinite time-out period
-            
+            Socket RevievedClientSocket = (Socket)obj;
+            string WelcomeMessage = "Welcome To My Server";
+            byte[] data = Encoding.ASCII.GetBytes(WelcomeMessage);
+            RevievedClientSocket.Send(data);
+            int recievedLength;
+            RevievedClientSocket.ReceiveTimeout = 0;
             // TODO: receive requests in while true until remote client closes the socket.
             while (true)
             {
                 try
                 {
                     // TODO: Receive request
-
+                    data = new byte[1024];
+                    recievedLength = RevievedClientSocket.Receive(data);
                     // TODO: break the while loop if receivedLen==0
-
+                    if (recievedLength==0)
+                    {
+                        Console.WriteLine("Client with IP:{0} ended the connection with us :(", RevievedClientSocket.RemoteEndPoint);
+                        break;
+                    }
                     // TODO: Create a Request object using received request string
-
+                    Request req = new Request(Encoding.ASCII.GetString(data));
                     // TODO: Call HandleRequest Method that returns the response
-
+                    
                     // TODO: Send Response back to client
 
                 }
@@ -59,6 +78,7 @@ namespace HTTPServer
             }
 
             // TODO: close client socket
+            RevievedClientSocket.Close();
         }
 
         Response HandleRequest(Request request)
